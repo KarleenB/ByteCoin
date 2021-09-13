96ef7a133add1085fdefddee8eab44c2cf2a0a1e
Branden Karleen <karleen@MacBook-Karleen.local>
2021-09-13 16:19:11 -0700

    Initial Commit

---
+++ ByteCoin/APIKey.swift
@@ -0,0 +1,13 @@
+//
+//  APIKey.swift
+//  ByteCoin
+//
+//  Created by Branden Karleen on 9/13/21.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct APIKey {
+    static let apiKey = "40C5E8FF-44A5-4100-AD97-3E69115BB3D9"
+}
---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,14 @@
+//
+//  CurrencyData.swift
+//  ByteCoin
+//
+//  Created by Branden Karleen on 6/25/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Codable {
+    let rate: Double
+}
+
